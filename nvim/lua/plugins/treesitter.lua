d12925e4cbb0b2891d1bb2f32e9e5739398089bd
Dorian-MB <dorian.mariani@gmail.com>
2026-08-21 00:29:56 +0200

    move treesitter to it own file

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,36 @@
+return {
+          "nvim-treesitter/nvim-treesitter",
+          branch = "main",
+          lazy = false,
+          build = ":TSUpdate",
+
+          config = function()
+               require("nvim-treesitter").install({
+                    "vim",
+                    "lua",
+                    "vimdoc",
+                    "html",
+                    "css",
+                    "rust",
+                    "python",
+                    "markdown",
+                    "markdown_inline",
+               })
+
+               vim.api.nvim_create_autocmd("FileType", {
+                    pattern = {
+                         "vim",
+                         "lua",
+                         "vimdoc",
+                         "html",
+                         "css",
+                         "rust",
+                         "python",
+                         "markdown",
+                    },
+                    callback = function(args)
+                         vim.treesitter.start(args.buf)
+                    end,
+               })
+          end,
+     }
