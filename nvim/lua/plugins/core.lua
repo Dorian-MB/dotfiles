be4e3845e84e3fa1953c801e1d60e569460c195c
Dorian-MB <dorian.mariani@gmail.com>
2026-08-23 00:03:49 +0200

    move rustaceanvim to it own file

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -30,19 +30,6 @@ return {
     { import = "nvchad.blink.lazyspec" },
 
     {
-        "mrcjkb/rustaceanvim",
-        -- To avoid being surprised by breaking changes,
-        -- I recommend you set a version range
-        version = "^9",
-        -- This plugin implements proper lazy-loading (see :h lua-plugin-lazy).
-        -- No need for lazy.nvim to lazy-load it.
-        lazy = false,
-        cargo = {
-            allfeatures = true,
-        },
-    },
-
-    {
         "jiaoshijie/undotree",
         opts = {
             -- your options
