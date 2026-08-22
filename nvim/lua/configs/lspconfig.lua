e93f35bd41f46d444323e16cad257e8c321fdd10
Dorian-MB <dorian.mariani@gmail.com>
2026-08-22 19:09:04 +0200

    add python to lsp server

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,12 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+          "html",
+          "cssls",
+          "pyright",
+          -- lua_ls by defalut with nvchad
+          -- rust-analyzer with rustaceanvim
+ }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
