d8c54d3ecd7e223b94432031338f140e95ff72d3
Dorian-MB <dorian.mariani@gmail.com>
2026-08-22 19:44:44 +0200

    add formarter via conform

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,71 +1,68 @@
 return {
-     {
-          "stevearc/conform.nvim",
-          -- event = 'BufWritePre', -- uncomment for format on save
-          opts = require "configs.conform",
-     },
+  { -- formatteur
+    "stevearc/conform.nvim",
+    cmd = "ConformInfo",
+    event = "BufWritePre", -- uncomment for format on savecore
+    opts = require "configs.conform",
+  },
 
-     -- These are some examples, uncomment them if you want to see them work!
-     {
-          "neovim/nvim-lspconfig",
-          config = function()
-               require "configs.lspconfig"
-          end,
-     },
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
 
-     {
-          "kylechui/nvim-surround",
-          version = "*", -- Use for stability; omit to use `main` branch for the latest features
-          event = "VeryLazy",
-          config = function()
-               require("nvim-surround").setup({
-                    -- Configuration here, or leave empty to use defaults
-               })
-          end
-     },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 
-     -- test new blink
-     { import = "nvchad.blink.lazyspec" },
+  -- test new blink
+  { import = "nvchad.blink.lazyspec" },
 
+  {
+    "mrcjkb/rustaceanvim",
+    -- To avoid being surprised by breaking changes,
+    -- I recommend you set a version range
+    version = "^9",
+    -- This plugin implements proper lazy-loading (see :h lua-plugin-lazy).
+    -- No need for lazy.nvim to lazy-load it.
+    lazy = false,
+    cargo = {
+      allfeatures = true,
+    },
+  },
 
-     {
-          'mrcjkb/rustaceanvim',
-          -- To avoid being surprised by breaking changes,
-          -- I recommend you set a version range
-          version = '^9',
-          -- This plugin implements proper lazy-loading (see :h lua-plugin-lazy).
-          -- No need for lazy.nvim to lazy-load it.
-          lazy = false,
-          cargo = {
-               allfeatures = true,
-          }
-     },
+  {
+    "jiaoshijie/undotree",
+    opts = {
+      -- your options
+    },
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
 
-
-     {
-          "jiaoshijie/undotree",
-          opts = {
-               -- your options
-          },
-          keys = { -- load the plugin only when using it's keybinding:
-               { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>"},
-          },
-     },
-
-     {
-         "lewis6991/gitsigns.nvim",
-         event = "BufReadPost",
-         opts = {},
-         keys = {
-             {
-                 "<leader>gd",
-                 function()
-                     require("gitsigns").diffthis("HEAD")
-                 end,
-                 desc = "git diff current file",
-             },
-         },
-     }
-
-
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPost",
+    opts = {},
+    keys = {
+      {
+        "<leader>gd",
+        function()
+          require("gitsigns").diffthis "HEAD"
+        end,
+        desc = "git diff current file",
+      },
+    },
+  },
 }
